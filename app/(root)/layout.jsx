32231d8ade01631482f7dff01ed4144a93fa9f3c
gtathelegend <gta.thelegend@gmail.com>
2025-11-04 03:09:46 +0530

    Update layout.jsx

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,14 +1,14 @@
 "use client"
 import Sidebar from "@/components/Sidebar";
 import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function RootLayout({ children }) {
 	return (
 		<>
 			<Sidebar />
 			<Analytics />
-			<SpeedInsights />
+			
 			{children}
 		</>
 	);
